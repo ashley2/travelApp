356ef14f04a67b14bd1f50a040176d73eeef61c1
Ashley Smith <ashley22@vt.edu>
2016-04-13 15:09:52 -0700

    fix broken components

--- public/services/Dest.js
+++ public/services/Dest.js
@@ -4,23 +4,13 @@ var app = angular.module('destApp');
 
 app.service('DestService', function($http){
 
-  this.getAll = function(){
-    return $http.get('/destinations')
-  };
+  this.getAll = () => $http.get('/destinations')
 
-  this.create = function(newDest){
-    return $http.post('/destinations', newDest);
-  }
+  this.create = (newDest) => $http.post('/destinations', newDest);
 
-  this.delete = function(dest) {
-   return $http.delete(`/destinations/${dest._id}`)
+  this.delete = (dest)  => $http.delete(`/destinations/${dest._id}`)
 
-
- };
-
- this.update = function(editDest){
-    return $http.put('/destinations', editDest);
-};
+  this.update = (editDest) => $http.put('/destinations', editDest);
 })
 
 
